23439f0b1a29c43c59a5e108821028190a0aa37a
Brian Roberts <brian.roberts@timgroup.com>
2015-11-03 20:57:37 +0100

    Complete!  Also try with BigInt

--- 2015-11-Primes/src/main/scala/main.scala
+++ 2015-11-Primes/src/main/scala/main.scala
@@ -1,9 +1,23 @@
 package Dojo
 
 object Main {
-  def find44thPrime : Int = ???
+  val sieve : Stream[Int] = {
+    2 #:: Stream.from(3, 2).filter { i =>
+      !sieve.takeWhile(j => j * j <= i)
+            .exists(j => i % j == 0)
+    }
+  }
 
-  def sieve : Stream[Int] = Stream(2, 3, 5)
+  val sieveBig : Stream[BigInt] = {
+    BigInt(2) #:: bigIntFrom(3, 2).filter { i =>
+      !sieveBig.takeWhile(j => j * j <= i)
+               .exists(j => i % j == 0)
+    }
+  }
+
+  def bigIntFrom(from: BigInt, step: BigInt): Stream[BigInt] = {
+    from #:: bigIntFrom(from + step, step)
+  }
 
   def isPrime(i:Int) : Boolean =
     sieve.takeWhile(_<=i).contains(i)
--- 2015-11-Primes/src/test/scala/Test.scala
+++ 2015-11-Primes/src/test/scala/Test.scala
@@ -17,8 +17,41 @@ class PrimeSpec extends FunSpec with Matchers {
     assert(isPrime(5))
   }
 
-  it("the 44th prime after 100,000 exists") {
-    pending
+  it("9 is not a prime") {
+    assert(!isPrime(9))
   }
+
+  it("13 is a prime") {
+    assert(isPrime(13))
+  }
+
+  it("the 44th prime after 100,000 exists") {
+    val thePrime = sieve.dropWhile(_ <= 100000)
+      .drop(43)
+      .head
+
+    println("The prime is: " + thePrime)
+    assert(thePrime > 100000)
+  }
+
+  it("bonus: the next four primes after the 44th prime after 100,000") {
+    val theNextPrimes = sieve.dropWhile(_ <= 100000)
+      .drop(43)
+      .drop(1)
+      .take(4)
+      .toList
+
+    println("The next primes are: " + theNextPrimes)
+    assert(theNextPrimes.size == 4)
+  }
+
+  /*
+  it("can it do max int?") {
+    val largeNumber = Int.MaxValue / 2
+    println("Trying to find a big prime larger than: " + largeNumber)
+    val prime = sieveBig.dropWhile(_ < largeNumber).head
+    println("A prime: " + prime)
+  }
+  */
 
 }
