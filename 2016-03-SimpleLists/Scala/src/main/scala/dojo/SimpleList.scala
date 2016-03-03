2a3e3fb02cd86f361d768bbaf88004c45537a6fd
Brian Roberts <brian.roberts@timgroup.com>
2016-03-03 19:28:10 +0100

    Add outline of tonight's dojo.

---
+++ 2016-03-SimpleLists/Scala/src/main/scala/dojo/SimpleList.scala
@@ -0,0 +1,4 @@
+package dojo
+
+object SimpleList {
+} 
---
+++ 2016-03-SimpleLists/Scala/src/test/scala/dojo/SimpleListSpec.scala
@@ -0,0 +1,11 @@
+package dojo
+
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+import org.scalatest.{DiagrammedAssertions, FunSpec}
+
+import scala.io.Source
+
+class SimpleListSpec extends FunSpec with DiagrammedAssertions {
+}
